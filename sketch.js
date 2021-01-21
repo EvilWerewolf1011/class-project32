72f30d4b079faed5fd2d5d4aca2b7dde4ff1be4a
Ayush Sawant <71120478+yush1011@users.noreply.github.com>
2021-01-21 18:09:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,140 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+var bg, bgDefault;
+var score = 0;
+
+var gameState = "onSling";
+
+function preload() {
+   bgDefault = loadImage("sprites/bg.png");
+
+   getBackgroundImage();
+
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+}
+
+function draw(){
+    if (backgroundImg) {
+        background(backgroundImg);
+    }
+    else{
+        background(bgDefault);
+    }
+
+    Engine.update(engine);
+
+    textSize(20)
+    fill("white")
+    text("Score : " + score, width-300,50);
+
+
+  
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    pig1.score();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    pig3.score();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    gameState = "launched";
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+       // slingshot.attach(bird.body);
+    }
+}
+
+async function getBackgroundImage(){
+
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Tokyo")
+    var responseJSON = await response.json();
+    var datetime =  responseJSON.datetime;
+
+    var hour = datetime.slice(11,13);
+
+    if(hour >= 06 && hour <= 19){
+        bg = "sprites/bg.png";
+    }else{
+        bg = "sprites/bg2.jpg";
+    }
+    
+    console.log(bg);
+
+    backgroundImg = loadImage(bg);
+
+    console.log(backgroundImg);
+
+    
+
+}
+/*
+{
+    "abbreviation":"IST",
+    "client_ip":"106.210.150.231",
+    "datetime":"2021-01-21T17:20:46.234879+05:30",
+    "day_of_week":4,"day_of_year":21,"dst":false,"dst_from":null,"dst_offset":0,"dst_until":null,"raw_offset":19800,"timezone":"Asia/Kolkata","unixtime":1611229846,"utc_datetime":"2021-01-21T11:50:46.234879+00:00","utc_offset":"+05:30","week_number":3}
+*/
+
